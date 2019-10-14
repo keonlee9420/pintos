63ba82a3cb2aedebecf6a034f3106a3b50091ba4
KeonLee <rjsdlwwkd@kaist.ac.kr>
2019-10-14 15:37:52 +0900

    start applying vaddr

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -147,6 +147,15 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
+	
+	/* Project2 S */
+	if (!user)
+	{
+		f->eip = (void*)f->eax;
+		f->eax = 0xffffffff;
+		return;		
+	}
+	/* Project2 E */
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,8 +4,13 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+static int get_user (const uint8_t *uaddr);
+static int get_user_page (void *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
+static void sys_exit (int status);
 
 void
 syscall_init (void) 
@@ -18,8 +23,8 @@ syscall_handler (struct intr_frame *f)
 {
 	/* Project2 S */
 	int *esp = f->esp;
-	int syscall_num = *esp;
-  // printf ("SYSCALL NUM: %d\n", syscall_num);
+	int syscall_num = get_user_page (esp);
+	// printf ("SYSCALL NUM: %d\n", syscall_num);
 	
 	switch (syscall_num)
 	{
@@ -30,15 +35,14 @@ syscall_handler (struct intr_frame *f)
 			}
 		case SYS_EXIT:
 			{
-				int status = *(int *)(esp + 1);
-				printf ("%s: exit(%d)\n", thread_name (), status);
-				thread_exit ();
+				int status = get_user_page ((int *)(esp + 1));
+				sys_exit (status);
 				break;
 			}
-
+		
 		case SYS_WRITE:
 			{
-				void *buffer = *(void **)(esp + 2);
+				void *buffer = (void *) get_user_page ((void **)(esp + 2));
 				printf ("%s", (char *)buffer);
 				break;
 			}
@@ -47,3 +51,54 @@ syscall_handler (struct intr_frame *f)
 	}	
 	/* Project2 E */
 }
+
+/* Project2 S */
+/* Reads a byte at user virtual address UADDR.
+UADDR must be below PHYS_BASE.
+Returns the byte value if successful, -1 if a segfault
+occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+	int result;
+	asm ("movl $1f, %0; movzbl %1, %0; 1:"
+	: "=&a" (result) : "m" (*uaddr));
+	return result;
+}
+
+static int
+get_user_page (void *uaddr)
+{
+	int page_value = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (!is_user_vaddr (uaddr + i))
+			sys_exit (-1);
+		int at_value = get_user (uaddr + i);
+		if (at_value == -1)
+			sys_exit (-1);
+		page_value += at_value << (8 * i);
+	}
+	return page_value;
+}
+
+/* Writes BYTE to user address UDST.
+UDST must be below PHYS_BASE.
+Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+	int error_code;
+	asm ("movl $1f, %0; movb %b2, %1; 1:"
+	: "=&a" (error_code), "=m" (*udst) : "q" (byte));
+	return error_code != -1;
+}
+
+static void 
+sys_exit (int status)
+{
+	printf ("%s: exit(%d)\n", thread_name (), status);
+	thread_exit ();
+}
+/* Project2 E */
